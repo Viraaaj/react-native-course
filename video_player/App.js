b55cdc8a71422abaca5aabf6545f3d6bb47f1cda
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-01-18 17:28:02 +0530

    Added Splash Screen and filters

--- video_player/App.js
+++ video_player/App.js
@@ -3,7 +3,7 @@ import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 import { Button, Container, Content, List, ListItem, Text, View } from 'native-base';
 import { createStackNavigator } from '@react-navigation/stack';
 import Video, { FilterType } from 'react-native-video';
-import { Animated, StyleSheet } from 'react-native';
+import { Animated, ImageBackground, StyleSheet } from 'react-native';
 import { useState } from 'react';
 import Swipeable from 'react-native-gesture-handler/Swipeable'
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -17,176 +17,248 @@ function VideoListScreen({navigation}) {
         <List>
 
           <ListItem 
-            onPress={() => navigation.navigate('Video Player', {
+            onPress={() => navigation.navigate('Video List', {
               external: true,
               videoURL: 'https://www.w3schools.com/html/mov_bbb.mp4'
             })} >
-              <Text style={styles.text}> 
-                External Video 
+              <Text style={styles.text1}> 
+                Videos List 
               </Text>
           </ListItem>
 
 
+          <Swipeable renderLeftActions={ (progress, dragX) => {
+              const scale = dragX.interpolate({
+                inputRange: [0, 100],
+                outputRange: [0, 1],
+                extrapolate: 'clamp'
+              })
+              return (
+                <View
+                  style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center', borderBottomColor: "#ffffff", borderWidth:0.5, }}>
+                  <Animated.Text 
+                    onPress={() => navigation.navigate('Video Player', {
+                    external: false,
+                    videoURL: require('./react.mp4')
+                    })}
+                    style={{
+                      color: '#00CC99',
+                      paddingHorizontal: 10,
+                      fontWeight: 'bold',
+                      fontSize: 20,
+                      left: 18,
+                      transform: [{ scale }]
+                    }}>
+                    Open Video
+                  </Animated.Text>
+                </View>
+              )
+            }}>
 
-          {/* <ListItem
-            onPress={() => navigation.navigate('Video Player', {
-            external: false,
-            videoURL: require('./react.mp4')
-            })} >
-              <Text style={styles.text}> React-Native </Text>
-          </ListItem>
-
-          <ListItem 
-            onPress={() => navigation.navigate('Video Player', {
-            external: false,
-            videoURL: require('./react.mp4')
-            })} >
-              <Text style={styles.text}> React.js </Text>
-          </ListItem>
-
-          <ListItem 
-            onPress={() => navigation.navigate('Video Player', {
-            external: false,
-            videoURL: require('./react.mp4')
-            })} >
-              <Text style={styles.text}> React-Redux </Text>
-          </ListItem>
-
-          <ListItem 
-            onPress={() => navigation.navigate('Video Player', {
-            external: false,
-            videoURL: require('./react.mp4')
-            })} >
-              <Text style={styles.text}> React-Hooks </Text>
-          </ListItem>
-
-          <ListItem 
-            onPress={() => navigation.navigate('Video Player', {
-            external: false,
-            videoURL: require('./react.mp4')
-            })} >
-              <Text style={styles.text}> React-Native libraries </Text>
-          </ListItem>
-
-          <ListItem 
-            onPress={() => navigation.navigate('Video Player', {
-            external: false,
-            videoURL: require('./react.mp4')
-            })} >
-              <Text style={styles.text}> Django </Text>
-          </ListItem>
-
-          <ListItem 
-            onPress={() => navigation.navigate('Video Player', {
-            external: false,
-            videoURL: require('./react.mp4')
-            })} >
-              <Text style={styles.text}> Complete web development </Text>
-          </ListItem> */}
-
-          
-          {/* <Swipeable renderLeftActions={LeftActions} >
-            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}
-              onPress={() => navigation.navigate('Video Player', {
-                external: false,
-                videoURL: require('./react.mp4')
-                })}
-               >
+            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}>
                 <Text style={styles.text}> React-Native </Text>
             </ListItem>
           </Swipeable>
 
-          <Swipeable renderLeftActions={LeftActions} >
-            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}
-              onPress={() => navigation.navigate('Video Player', {
-                external: false,
-                videoURL: require('./react.mp4')
-                })}
-               >
+
+          <Swipeable renderLeftActions={ (progress, dragX) => {
+              const scale = dragX.interpolate({
+                inputRange: [0, 100],
+                outputRange: [0, 1],
+                extrapolate: 'clamp'
+              })
+              return (
+                <View
+                  style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center', borderBottomColor: "#ffffff", borderWidth:0.5, }}>
+                  <Animated.Text 
+                    onPress={() => navigation.navigate('Video Player', {
+                    external: false,
+                    videoURL: require('./react.mp4')
+                    })}
+                    style={{
+                      color: '#00CC99',
+                      paddingHorizontal: 10,
+                      fontWeight: 'bold',
+                      fontSize: 20,
+                      left: 18,
+                      transform: [{ scale }]
+                    }}>
+                    Open Video
+                  </Animated.Text>
+                </View>
+              )
+            }}>
+
+            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}>
                 <Text style={styles.text}> React.js </Text>
             </ListItem>
           </Swipeable>
 
-          <Swipeable renderLeftActions={LeftActions} >
-            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}
-              onPress={() => navigation.navigate('Video Player', {
-                external: false,
-                videoURL: require('./react.mp4')
-                })}
-               >
+
+          <Swipeable renderLeftActions={ (progress, dragX) => {
+              const scale = dragX.interpolate({
+                inputRange: [0, 100],
+                outputRange: [0, 1],
+                extrapolate: 'clamp'
+              })
+              return (
+                <View
+                  style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center', borderBottomColor: "#ffffff", borderWidth:0.5, }}>
+                  <Animated.Text 
+                    onPress={() => navigation.navigate('Video Player', {
+                    external: false,
+                    videoURL: require('./react.mp4')
+                    })}
+                    style={{
+                      color: '#00CC99',
+                      paddingHorizontal: 10,
+                      fontWeight: 'bold',
+                      fontSize: 20,
+                      left: 18,
+                      transform: [{ scale }]
+                    }}>
+                    Open Video
+                  </Animated.Text>
+                </View>
+              )
+            }}>
+
+            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}>
                 <Text style={styles.text}> React-Redux </Text>
             </ListItem>
           </Swipeable>
 
-          <Swipeable renderLeftActions={LeftActions} >
-            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}
-              onPress={() => navigation.navigate('Video Player', {
-                external: false,
-                videoURL: require('./react.mp4')
-                })}
-               >
+
+          <Swipeable renderLeftActions={ (progress, dragX) => {
+              const scale = dragX.interpolate({
+                inputRange: [0, 100],
+                outputRange: [0, 1],
+                extrapolate: 'clamp'
+              })
+              return (
+                <View
+                  style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center', borderBottomColor: "#ffffff", borderWidth:0.5, }}>
+                  <Animated.Text 
+                    onPress={() => navigation.navigate('Video Player', {
+                    external: false,
+                    videoURL: require('./react.mp4')
+                    })}
+                    style={{
+                      color: '#00CC99',
+                      paddingHorizontal: 10,
+                      fontWeight: 'bold',
+                      fontSize: 20,
+                      left: 18,
+                      transform: [{ scale }]
+                    }}>
+                    Open Video
+                  </Animated.Text>
+                </View>
+              )
+            }}>
+
+            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}>
                 <Text style={styles.text}> React-Hooks </Text>
             </ListItem>
           </Swipeable>
 
-          <Swipeable renderLeftActions={LeftActions} >
-            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}
-              onPress={() => navigation.navigate('Video Player', {
-                external: false,
-                videoURL: require('./react.mp4')
-                })}
-               >
-                <Text style={styles.text}> React-Native libraries </Text>
-            </ListItem>
-          </Swipeable>
 
-          <Swipeable renderLeftActions={LeftActions} >
-            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}
-              onPress={() => navigation.navigate('Video Player', {
-                external: false,
-                videoURL: require('./react.mp4')
-                })}
-               >
+          <Swipeable renderLeftActions={ (progress, dragX) => {
+              const scale = dragX.interpolate({
+                inputRange: [0, 100],
+                outputRange: [0, 1],
+                extrapolate: 'clamp'
+              })
+              return (
+                <View
+                  style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center', borderBottomColor: "#ffffff", borderWidth:0.5, }}>
+                  <Animated.Text 
+                    onPress={() => navigation.navigate('Video Player', {
+                    external: false,
+                    videoURL: require('./react.mp4')
+                    })}
+                    style={{
+                      color: '#00CC99',
+                      paddingHorizontal: 10,
+                      fontWeight: 'bold',
+                      fontSize: 20,
+                      left: 18,
+                      transform: [{ scale }]
+                    }}>
+                    Open Video
+                  </Animated.Text>
+                </View>
+              )
+            }}>
+
+            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}>
                 <Text style={styles.text}> Django </Text>
             </ListItem>
           </Swipeable>
 
-          <Swipeable renderLeftActions={LeftActions} >
-            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}
-              onPress={() => navigation.navigate('Video Player', {
-                external: false,
-                videoURL: require('./react.mp4')
-                })}
-               >
+
+          <Swipeable renderLeftActions={ (progress, dragX) => {
+              const scale = dragX.interpolate({
+                inputRange: [0, 100],
+                outputRange: [0, 1],
+                extrapolate: 'clamp'
+              })
+              return (
+                <View
+                  style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center', borderBottomColor: "#ffffff", borderWidth:0.5, }}>
+                  <Animated.Text 
+                    onPress={() => navigation.navigate('Video Player', {
+                    external: false,
+                    videoURL: require('./react.mp4')
+                    })}
+                    style={{
+                      color: '#00CC99',
+                      paddingHorizontal: 10,
+                      fontWeight: 'bold',
+                      fontSize: 20,
+                      left: 18,
+                      transform: [{ scale }]
+                    }}>
+                    Open Video
+                  </Animated.Text>
+                </View>
+              )
+            }}>
+
+            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}>
                 <Text style={styles.text}> Complete web development </Text>
             </ListItem>
-          </Swipeable> */}
-
-          
+          </Swipeable>
         
-          <Swipeable renderLeftActions={ () => {
-  return (
-    <View
-      style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center' }}>
-      <Text onPress={() => navigation.navigate('Video Player', {
-                external: false,
-                videoURL: require('./react.mp4')
-                })}
-        style={{
-          color: '#ff0000',
-          paddingHorizontal: 10,
-          fontWeight: 'bold',
-          fontSize: 20,
-          left: 18
-        }}>
-        Open Video
-      </Text>
-    </View>
-  )
- }} >
-            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}
-            
-               >
+          <Swipeable renderLeftActions={ (progress, dragX) => {
+              const scale = dragX.interpolate({
+                inputRange: [0, 100],
+                outputRange: [0, 1],
+                extrapolate: 'clamp'
+              })
+              return (
+                <View
+                  style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center', borderBottomColor: "#ffffff", borderWidth:0.5, }}>
+                  <Animated.Text 
+                    onPress={() => navigation.navigate('Video Player', {
+                    external: false,
+                    videoURL: require('./react.mp4')
+                    })}
+                    style={{
+                      color: '#00CC99',
+                      paddingHorizontal: 10,
+                      fontWeight: 'bold',
+                      fontSize: 20,
+                      left: 18,
+                      transform: [{ scale }]
+                    }}>
+                    Open Video
+                  </Animated.Text>
+                </View>
+              )
+            }}>
+
+            <ListItem style={{ flex: 1, backgroundColor:"#000000"}}>
                 <Text style={styles.text}> Complete mobile development </Text>
             </ListItem>
           </Swipeable>
@@ -197,7 +269,26 @@ function VideoListScreen({navigation}) {
   )
 }
 
-// const LeftActions =
+// const LeftActions = () => {
+//   return (
+//     <View
+//       style={{ flex: 1, backgroundColor: '#000000', justifyContent: 'center' }}>
+//       <Text onPress={() => navigation.navigate('Video Player', {
+//                 external: false,
+//                 videoURL: require('./react.mp4')
+//                 })}
+//         style={{
+//           color: '#ff0000',
+//           paddingHorizontal: 10,
+//           fontWeight: 'bold',
+//           fontSize: 20,
+//           left: 18
+//         }}>
+//         Open Video
+//       </Text>
+//     </View>
+//   )
+// }
 
 // const LeftActions = (progress, dragX) => {
 //   const scale = dragX.interpolate({
@@ -227,29 +318,87 @@ function VideoListScreen({navigation}) {
 function VideoPlayerScreen({navigation, route}) {
 
   const {external, videoURL} = route.params;
-  const [filterType, setFilterType] = React.useState(FilterType.MAXIMUMCOMPONENT)
+  const [filterType, setFilterType] = React.useState(FilterType.FADE)
+
+  const changeFilter = filterType => {
+    setFilterType(filterType);
+  }
 
   return <Container style={{backgroundColor:"#000000"}}>
 
     <Video
       controls
       paused
-      filterEnable
+      filterEnabled={true}
       filter={filterType}
       source={external ? {uri: videoURL} : videoURL}
-      style={{ top:0, left:22, bottom:0, right:0, position:"absolute", marginVertical:"50%", width:"90%" }}
+      resizeMode="contain"
+      style={{ top:0, left:0, bottom:0, right:0, position:"absolute", marginVertical:"50%"}}
     />
 
-    {/* <Button 
-      style = {{ marginTop: 500 }}
-      block
-      onPress={() => {}} 
+    <Text
+      onPress={() => navigation.navigate('Video List')} 
+      style={{color:"#ffffff", fontWeight:"bold", textAlign:"right", right:16, top:12}}
     >
-      <Text>  </Text>
-    </Button> */}
+    Close
+    </Text>
+
+
+    <View style={{flex:1, marginTop:50}}>
+      <Button 
+        block
+        onPress={() => {changeFilter(FilterType.MONO)} } 
+      >
+        <Text>
+          Change filter to MONO 
+        </Text>
+      </Button>
+
+      <Button 
+        block
+        onPress={() => {changeFilter(FilterType.CHROME)}} 
+      >
+        <Text>
+          Change filter to CHROME 
+        </Text>
+      </Button>
+
+      <Button 
+        block
+        onPress={() => {changeFilter(FilterType.SEPIA)}} 
+      >
+        <Text>
+          Change filter to SEPIA 
+        </Text>
+      </Button>
+    </View>
     
   </Container>
 }
+
+function SplashScreen({navigation}) {
+
+  setTimeout(() => {
+    navigation.replace('Video List')
+  }, 1500);
+
+  return (
+    <ImageBackground style={{flex:1}} source={{uri:'https://camo.githubusercontent.com/26e52bf82450c26eab49c9db7bfc7c62d516afe77d81eb2f0706ef31cecb7b37/68747470733a2f2f7777772e66696c657069636b65722e696f2f6170692f66696c652f55763031334c6e77527a6d32485848426b396951'}} >
+    </ImageBackground>
+  )
+}
+
+// function SplashScreenVideo({navigation}) {
+
+//   setTimeout(() => {
+//     navigation.replace('Video List')
+//   }, 1500);
+
+//   return (
+//     <Video style={{flex:1}} source={require('./react.mp4')} resizeMode="contain"/>
+//   )
+// }
+
 
 const Stack = createStackNavigator();
 
@@ -274,16 +423,17 @@ export default class App extends Component {
        
     };
     navigation = this.props.navigation;
-    // this.LeftActions = this.LeftActions.bind(this)
   }
   
 
   render() {
     return (
       <NavigationContainer theme={DarkTheme}>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='SplashScreen' headerMode='none'>
         <Stack.Screen name="Video List" component={VideoListScreen} />
         <Stack.Screen name="Video Player" component={VideoPlayerScreen} />
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        {/* <Stack.Screen name="SplashScreenVideo" component={SplashScreenVideo} /> */}
       </Stack.Navigator>
     </NavigationContainer>
     )
@@ -296,6 +446,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight:"bold",
     color: "#ff0000",
+    alignSelf:"center"
+  },
+  text1:{
+    textAlign:"center",
+    fontSize: 20,
+    fontWeight:"bold",
+    color: "#ffffff",
     alignSelf:"center"
   }
 })
